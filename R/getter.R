7036fc86746fca8e854119860784ec39313ad012
trestletech <jeff.allen@trestletechnology.net>
2013-04-15 15:10:23 -0500

    Fixed documentation bugs.
    Closes #15.

--- R/getter.R
+++ R/getter.R
@@ -1,7 +1,7 @@
 #' Generic class to retrieve objects
 #' 
 #' Define a getter class to specify how objects should be retrieved.
-#' @alises Getter-class
+#' @aliases Getter-class
 #' @exportClass Getter
 #' @author Jeffrey D. Allen \email{Jeffrey.Allen@@UTSouthwestern.edu}
 Getter <- setRefClass("Getter", 
